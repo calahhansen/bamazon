389213f3a1eae9f928f707ea8bc3521d7e4403ec
Calah Hansen <52944732+calahhansen@users.noreply.github.com>
2019-09-16 17:43:22 -0500

    customer product display
    displays/reads the products that are seeded initially into the database

---
+++ bamazonCustomer.js
@@ -0,0 +1,41 @@
+//running this application will first display all of the items available for sale.
+//Include the ids, names, and prices of products for sale.
+
+//Require mysql and inquirer npm packages (Remember to install!)
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+//Set up the connection information required as an object
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "products_db"
+});
+
+//Use the connect method on the object "connection".  The method argument is a callback function that will result in an error or console log that the database is connected.
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  displayProduct();
+});
+// Display/Read all of the product items for sale and include ids, product names, depts, prices and quantity of product
+function displayProduct() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log(res);
+
+    // logs the actual query being run
+    console.log(query.sql);
+
+    //END of the connection.
+    connection.end();
+  });
+}
