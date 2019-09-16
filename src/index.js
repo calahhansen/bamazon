29031d66444678b3a8a8a2bace8eb8b58da2ed4d
Calah Hansen <52944732+calahhansen@users.noreply.github.com>
2019-09-16 12:51:46 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// Drops the bamazon_db if it exists currently //
+DROP DATABASE IF EXISTS bamazon_db;
+// Creates the "bamazon_db" database //
+CREATE DATABASE bamazon_db;
+
+// Makes it so all of the following code will affect bamazon_db //
+USE bamazon_db;
+
+//Creates the table "products" within bamazon_db --
+CREATE TABLE products (
+  // Makes an unique id for each product //
+  item_id INTEGER(8) AUTO_INCREMENT NOT NULL,
+  // Makes a string column called "product_name" which cannot contain null //
+  product_name VARCHAR(30) NOT NULL,
+  // Makes a string column called "department_name" which cannot contain null //
+  department_name VARCHAR(30) NOT NULL,
+  // Makes a decimal column called "price" //
+  price DECIMAL(10,2) NOT NULL,
+  // Makes an numeric column called "stock_quantity" //
+  stock_quantity INTEGER(8) NOT NULL,
+  // Makes item_id the primary key //
+  PRIMARY KEY (item_id)
+);
+
+//Creates new rows containing data in all named columns --
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity, PRIMARY KEY)
+VALUES ("Jenga", "Games & Accessories", 5.75, 100), 
+("LEGO Harry Potter Advent Calendar", "Games & Accessories", 32.99, 100),
+("Play-doh 10 pack case of colors", "Games & Accessories", 7.99, 100),
+("Jenga", "Games & Accessories", 5.75, 100),
+("Jenga", "Games & Accessories", 5.75, 100),
+("Jenga", "Games & Accessories", 5.75, 100),
+("Jenga", "Games & Accessories", 5.75, 100),
+("Jenga", "Games & Accessories", 5.75, 100),
+("Jenga", "Games & Accessories", 5.75, 100),
+("Jenga", "Games & Accessories", 5.75, 100),
+("Jenga", "Games & Accessories", 5.75, 100);
+
+
+
+//Updates the row where the column name is ??
+UPDATE products
+//SET has_pet = true, pet_name = "Franklin", pet_age = 2
+//WHERE name = "Peter";
+
